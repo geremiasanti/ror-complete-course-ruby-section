b67de36b8aae5660b10824acc8f40bbb25e87ce7
geremia <germysettesette@gmail.com>
2024-10-29 17:32:41 +0100

    area code dict

---
+++ area_code_dict.rb
@@ -0,0 +1,36 @@
+dial_book = {
+  "newyork" => "212",
+  "newbrunswick" => "732",
+  "edison" => "908",
+  "plainsboro" => "609",
+  "sanfrancisco" => "301",
+  "miami" => "305",
+  "paloalto" => "650",
+  "evanston" => "847",
+  "orlando" => "407",
+  "lancaster" => "717"
+}
+ 
+# Get city names from the hash
+def get_city_names(hash)
+  return hash.map { |cityname, _| "- #{cityname}" }.join("\n")
+end
+ 
+# Get area code based on given hash and key
+def get_area_code(hash, key)
+  return hash[key];
+end
+ 
+# Execution flow
+loop do
+  puts 'Do you want to lookup an area code based on a city name? (Y/N)'
+  answer = gets.chomp.downcase
+  break if answer != 'y'
+
+  puts get_city_names dial_book
+  print 'city: '
+  input = gets.chomp
+
+  area_code = get_area_code dial_book, input
+  puts "Area code for #{input} is: #{area_code}"
+end
